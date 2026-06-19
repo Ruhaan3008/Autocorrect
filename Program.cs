fb6b794bb9375d8a61f7b50191c8429ec3ffb418
Ruhaan Reddy <ruhaan3008@gmail.com>
2026-06-19 23:03:33 +0100

    Adding files

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+using Autocorrect;
+
+const string DICTIONARY_PATH = "D:/Projects/Autocorrect/dictionary.txt";
+
+WordDictionary dictionary = new WordDictionary(DICTIONARY_PATH);
+
+dictionary.PrecomputeWordLengthFrequency();
---
+++ WordDictionary.cs
@@ -0,0 +1,57 @@
+﻿namespace Autocorrect;
+
+public class WordDictionary
+{
+
+    private string[] words;
+
+    public string[] Words
+    {
+        get => words;
+        private set => words = value;
+    }
+
+    private int[] wordLengthFrequency;
+    
+    private int maxWordLength;
+
+    public int MaxWordLength
+    {
+        get => maxWordLength;
+        private set => maxWordLength = value;
+    }
+    
+    
+    public WordDictionary(string path)
+    {
+        words = File.ReadAllLines(path);
+        wordLengthFrequency = new int[0];
+        maxWordLength = 0;
+    }
+
+    public void PrecomputeWordLengthFrequency()
+    {
+        int largestWordLength = 0;
+        foreach (string word in words)
+        {
+            int wordLength = word.Length;
+            if (wordLength > largestWordLength)
+            {
+                largestWordLength = wordLength;
+            }
+        }
+        
+        maxWordLength = largestWordLength;
+        
+        wordLengthFrequency = new int[largestWordLength];
+        Array.Fill(wordLengthFrequency, 0);
+        
+        foreach (var word in words)
+        {
+            int wordLength = word.Length;
+            wordLengthFrequency[wordLength - 1] += 1;
+        }
+    }
+    
+    public int WordLengthFrequency(int length) => wordLengthFrequency[length - 1];
+}
