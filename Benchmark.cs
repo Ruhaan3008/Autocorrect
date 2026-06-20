28e5d3d6409dbf7b1f60405a1456b5a2ba3799ac
Ruhaan Reddy <ruhaan3008@gmail.com>
2026-06-20 01:51:49 +0100

    Added Benchmark Class

---
+++ Benchmark.cs
@@ -0,0 +1,25 @@
+﻿namespace Autocorrect;
+
+public class Benchmark
+{
+    public static string[] testWords = { "add", "rin", "tust", "weehy", "bruhh" };
+    const string DICTIONARY_PATH = "D:/Projects/Autocorrect/dictionary.txt";
+
+
+    public static void Test()
+    {
+        
+        WordDictionary dictionary = new WordDictionary(DICTIONARY_PATH);
+        
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        foreach (var word in testWords)
+        {
+            Corrector.ClosestWords(word, ref dictionary);
+        }
+        watch.Stop();
+        var elapsedMs = watch.ElapsedMilliseconds;
+        
+        Console.WriteLine($"Compute Time: {elapsedMs} ms");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,23 +3,31 @@ using Autocorrect;
 
 const string DICTIONARY_PATH = "D:/Projects/Autocorrect/dictionary.txt";
 
+var shouldBenchmark = true;
+
 WordDictionary dictionary = new WordDictionary(DICTIONARY_PATH);
 
 dictionary.PrecomputeWordLengthFrequency();
 
-
-Console.Write("Enter Word to Correct: ");
-string input =  Console.ReadLine().ToLower();
-
-
-var watch = System.Diagnostics.Stopwatch.StartNew();
-var words = Corrector.ClosestWords(input,ref dictionary);
-watch.Stop();
-var elapsedMs = watch.ElapsedMilliseconds;
-
-Console.WriteLine($"Compute Time: {elapsedMs} ms");
-Console.WriteLine($"Words found: {words.Length}");
-foreach (var word in words)
+if (shouldBenchmark)
 {
-    Console.WriteLine(word);
+    Benchmark.Test();
+}
+else
+{
+    Console.Write("Enter Word to Correct: ");
+    string input = Console.ReadLine().ToLower();
+
+
+    var watch = System.Diagnostics.Stopwatch.StartNew();
+    var words = Corrector.ClosestWords(input, ref dictionary);
+    watch.Stop();
+    var elapsedMs = watch.ElapsedMilliseconds;
+
+    Console.WriteLine($"Compute Time: {elapsedMs} ms");
+    Console.WriteLine($"Words found: {words.Length}");
+    foreach (var word in words)
+    {
+        Console.WriteLine(word);
+    }
 }
