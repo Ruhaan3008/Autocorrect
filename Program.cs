4881f1c8a7e73bb4302f0c87e36be1da41704300
Ruhaan Reddy <ruhaan3008@gmail.com>
2026-06-20 01:36:59 +0100

    Added time tracking for benchmarking

--- Program.cs
+++ Program.cs
@@ -7,8 +7,18 @@ WordDictionary dictionary = new WordDictionary(DICTIONARY_PATH);
 
 dictionary.PrecomputeWordLengthFrequency();
 
-var words = Corrector.ClosestWords("rin",ref dictionary);
 
+Console.Write("Enter Word to Correct: ");
+string input =  Console.ReadLine().ToLower();
+
+
+var watch = System.Diagnostics.Stopwatch.StartNew();
+var words = Corrector.ClosestWords(input,ref dictionary);
+watch.Stop();
+var elapsedMs = watch.ElapsedMilliseconds;
+
+Console.WriteLine($"Compute Time: {elapsedMs} ms");
+Console.WriteLine($"Words found: {words.Length}");
 foreach (var word in words)
 {
     Console.WriteLine(word);
